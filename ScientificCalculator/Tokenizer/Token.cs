2839fef4dc4c2d571859abdee986d648ce58bf88
Grzegorz Mierzwa <grzesiek.mierzwa@gmail.com>
2025-03-02 14:57:07 +0100

    Added implementation of IEquatable

--- ScientificCalculator/Tokenizer/Token.cs
+++ ScientificCalculator/Tokenizer/Token.cs
@@ -1,10 +1,27 @@
 namespace ScientificCalculator.Tokenizer;
 
-public abstract class Token
+public abstract class Token : IEquatable<Token>
 {
     public  string TextRepresentation { get; set; } = String.Empty;
+
+    public bool Equals(Token? other)
+    {
+        if (other is null) return false;
+        return TextRepresentation.Equals(other.TextRepresentation);
+    }
+
     public override string ToString()
     {
         return TextRepresentation;
-    } 
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Token t && Equals(t);
+    }
+
+    public override int GetHashCode()
+    {
+        return TextRepresentation.GetHashCode();
+    }
 }
